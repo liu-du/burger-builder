8ba110c87bfb41301b390ebdbebddd1afb578d05
liu-du <duliujimmy@hotmail.com>
2019-02-06 08:28:42 +1100

    Use css modules and inital layout

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <>
+        <div>I'm a burger!</div>
+        <div>Controls</div>
+      </>
+    );
+  }
+}
+
+export default BurgerBuilder;
