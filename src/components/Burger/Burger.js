04b18ad3944c803ae23b349058ce62c19259be32
liu-du <duliujimmy@hotmail.com>
2019-02-18 08:02:09 +1100

    Fix css media query

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,13 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.css";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igkey => {
-    return [...Array(props.ingredients[igkey])].map((_, i) => {
-      return <BurgerIngredient key={igkey + i} type={igkey} />;
-    });
-  });
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igkey => {
+      return [...Array(props.ingredients[igkey])].map((_, i) => {
+        return <BurgerIngredient key={igkey + i} type={igkey} />;
+      });
+    })
+    .flat();
   console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
