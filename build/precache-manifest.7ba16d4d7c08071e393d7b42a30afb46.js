e6b4b5bb38137ed6d12ff62f70fc653a50412433
liu-du <duliujimmy@hotmail.com>
2019-05-25 10:01:34 +1000

    Deploy

---
+++ build/precache-manifest.7ba16d4d7c08071e393d7b42a30afb46.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d948ed71824c08eb736e",
+    "url": "/static/css/main.860f4bad.chunk.css"
+  },
+  {
+    "revision": "d948ed71824c08eb736e",
+    "url": "/static/js/main.d948ed71.chunk.js"
+  },
+  {
+    "revision": "b53b1de95b164e93b8bf",
+    "url": "/static/css/1.47c69a26.chunk.css"
+  },
+  {
+    "revision": "b53b1de95b164e93b8bf",
+    "url": "/static/js/1.b53b1de9.chunk.js"
+  },
+  {
+    "revision": "74dc57a4ed87b84f2d96",
+    "url": "/static/css/2.e83b2aae.chunk.css"
+  },
+  {
+    "revision": "74dc57a4ed87b84f2d96",
+    "url": "/static/js/2.74dc57a4.chunk.js"
+  },
+  {
+    "revision": "82cea92b1a2e226fb87b",
+    "url": "/static/css/3.31c42a57.chunk.css"
+  },
+  {
+    "revision": "82cea92b1a2e226fb87b",
+    "url": "/static/js/3.82cea92b.chunk.js"
+  },
+  {
+    "revision": "9128b9308fca5ec5fbf8",
+    "url": "/static/js/4.9128b930.chunk.js"
+  },
+  {
+    "revision": "dd9abc823bf93684d290",
+    "url": "/static/js/runtime~main.dd9abc82.js"
+  },
+  {
+    "revision": "976da2e376c69896b93f0d80af1cbbf0",
+    "url": "/static/media/burger-logo.976da2e3.jpg"
+  },
+  {
+    "revision": "6e92dd408dee71cd38c3512cce2ccc35",
+    "url": "/index.html"
+  }
+];
